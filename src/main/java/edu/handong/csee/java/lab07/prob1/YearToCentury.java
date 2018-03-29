7aa8a17a65cac58951edd688960c3ba578861b4f
JeongSeop <wjdtjq1121@gmail.com>
2018-03-29 10:27:33 +0900

    first commit

---
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.java.lab07.prob1;
+
+public class YearToCentury {
+
+	public YearToCentury() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
