4a564c08b88be84d8d4e77da04571947ffe1d8d8
JeongSeop <wjdtjq1121@gmail.com>
2018-03-29 12:46:04 +0900

    finished first problem

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,14 +1,43 @@
 package edu.handong.csee.java.lab07.prob1;
 
+import java.util.Scanner;
+
 public class YearToCentury {
 
-	public YearToCentury() {
-		// TODO Auto-generated constructor stub
+	int year;
+	
+	public YearToCentury(int year) { // default constructor
+		this.year = year;
 	}
 
+	public int CalculateCentury() {
+		
+		int result;
+		int plusone=0;
+		result = year/100;
+		if(year%100 != 0)
+			plusone=1;
+		
+		
+		 result+=plusone;
+		
+		return result;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int yr=0;
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Input year: ");
+		yr = keyboard.nextInt();
+		
+		YearToCentury getCen = new YearToCentury(yr);
+		
+		System.out.println(yr + " is " + getCen.CalculateCentury() + "th century. ");
+		
+		
 	}
 
 }
